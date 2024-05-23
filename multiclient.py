1793fb39874d3c61af4b0b07b094c289d4dfca11
Rizqi Bhamaryuda <77719344+rizqibryda@users.noreply.github.com>
2024-05-23 22:40:26 +0700

    fixed v2

--- multiclient.py
+++ multiclient.py
@@ -1,68 +1,10 @@
-from multiprocessing import Process
-from socket import *
-import sys
+import subprocess
 
-def func1():
-    
-    # Menentukan nama host dan nomor port
-    serverName = "localhost"
-    serverPort = 8080
+# Daftar nama file program klien
+client_files = ["clientt.py", "clientt2.py"]
 
-    # Membuat Tcp Socket
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    # Menghubungi Server
-    clientSocket.connect((serverName, serverPort))
-    print("Server Connected")
+# Jalankan setiap program klien dalam proses terpisah
+for client_file in client_files:
+    subprocess.Popen(["python", client_file])
 
-    # Meminta input dari user
-    fileName = "/main.html"
-    request = "GET " + str(fileName)+ " HTTP/1.1"
-
-    # Mengirim pesan ke server
-    clientSocket.send(request.encode())
-
-    # Menerima pesan dari server
-    returnFromServer = clientSocket.recv(4096)
-
-    while (len(returnFromServer)> 0):
-        print(returnFromServer.decode())
-        returnFromServer = clientSocket.recv(4096)
-
-    # Menutup koneksi dengan server
-    clientSocket.close()
-    pass
-
-def func2():
-    # Menentukan nama host dan nomor port
-    serverName = "localhost"
-    serverPort = 8080
-
-    # Membuat Tcp Socket
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    # Menghubungi Server
-    clientSocket.connect((serverName, serverPort))
-    print("Server Connected")
-
-    # Meminta input dari user
-    fileName = "/main.html"
-    request = "GET " + str(fileName)+ " HTTP/1.1"
-
-    # Mengirim pesan ke server
-    clientSocket.send(request.encode())
-
-    # Menerima pesan dari server
-    returnFromServer = clientSocket.recv(4096)
-
-    while (len(returnFromServer)> 0):
-        print(returnFromServer.decode())
-        returnFromServer = clientSocket.recv(4096)
-
-    # Menutup koneksi dengan server
-    clientSocket.close()
-
-if __name__ == '__main__':
-    p1 = Process(target=func1)
-    p1.start()
-
-    p2 = Process(target=func2)
-    p2.start()
+print("Semua program klien telah dimulai.")
